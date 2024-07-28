9d954dc5d322f746366570408e94f929103c74ba
Bernardo <service.bsfg@gmail.com>
2024-07-28 04:19:55 -0500

    this is right 100%

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    }
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop;
+}
+function destructivelyRemoveFirstCat(){
+    cats.push;
+}
+function appendCat(name){
+    const cat = [...cats, "Broom"];
+}
+function prependCat(name){
+    const cat = ["Arnold", ...cats];
+}
+function removeLastCat(name){
+    const cat = [...cats];
+    cat.pop;
+}
+function removeFirstCat(name){
+    const cat = [...cats];
+    cat.push;
+}
